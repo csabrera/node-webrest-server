488d1a141274d4084d6a46b43a077a412d5eac16
Carlos Sabrera <csabreracaballero@gmail.com>
2024-04-02 16:07:59 -0500

    Tercer commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { envs } from "./config/envs";
 import { Server } from "./presentation/server";
+import { AppRoutes } from "./presentation/routes";
 
 (async () => {
   main();
@@ -8,6 +9,8 @@ import { Server } from "./presentation/server";
 function main() {
   const server = new Server({
     port: envs.PORT,
+    routes: AppRoutes.routes,
+    public_path: "public",
   });
   server.start();
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,26 +1,35 @@
-import express from "express";
+import express, { Router } from "express";
 import path from "path";
 
 interface Options {
   port: number;
-  public_path?: string;
+  routes: Router;
+  public_path: string;
 }
 
 export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, public_path = "public" } = options;
+    const { port, public_path, routes } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
 
   async start() {
     //* Middlewares
+    this.app.use(express.json()); //row
+    this.app.use(express.urlencoded({ extended: true })); //x-www-form-urlencoded
+
     //* Public Folder
     this.app.use(express.static(this.publicPath));
+
+    //* Routes
+    this.app.use(this.routes);
 
     this.app.get("*", (req, res) => {
       const indexPath = path.join(
